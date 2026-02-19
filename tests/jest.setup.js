0306d12e26fb5ceaca95cc8e872f513fa183f386
Jefti Vinicius Meira Dantas <1282420838@ulife.com.br>
2026-02-19 20:58:12 -0300

    feat: prisma

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,14 +1,10 @@
-// jest.setup.js
-require('../src/config/loadEnv'); // loads .env or .env.test
+require('../src/config/loadEnv');
 
 const prisma = require('../src/config/prisma');
 const { execSync } = require('child_process');
 
 beforeAll(async () => {
-  // Deploy migrations to the test database
   execSync('npx prisma migrate deploy', { stdio: 'inherit' });
-
-  // Clean tables
   await prisma.user.deleteMany();
 });
 
--- tests/user.test.js
+++ tests/user.test.js
@@ -4,8 +4,8 @@ const prisma = require('../src/config/prisma');
 
 describe('GET /api/users', () => {
   it('should return empty array initially', async () => {
-    const res = await request(app).get('/api/users');
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual([]);
-  });
-});
+    const res = await request(app).get('/api/users')
+    expect(res.status).toBe(200)
+    expect(res.body).toEqual([])
+  })
+})
